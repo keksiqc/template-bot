6987bf061468f3f7c7d6b6a095e8aff8a0a311a7
marlonhotz07 <marlonhotz07@gmail.com>
2025-07-23 08:35:01 +0000

    feat: add uvloop

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,4 +1,10 @@
 from bot.log import setup_logger
-
+import os
+import asyncio
 
 setup_logger()
+
+
+if os.name != "nt":
+    import uvloop
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
