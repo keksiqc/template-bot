f769bafd3ea96a9cde44a84dfdcce3d88a59e025
marlonhotz07 <marlonhotz07@gmail.com>
2025-07-23 08:35:32 +0000

    style: fix format

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,10 +1,13 @@
-from bot.log import setup_logger
-import os
 import asyncio
+import os
+
+from bot.log import setup_logger
+
 
 setup_logger()
 
 
 if os.name != "nt":
     import uvloop
+
     asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
